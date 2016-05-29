edfb721f37cfad4e601b7852094869f132e5eb5b
郭胜强 <guoshengqiang@live.com>
2016-05-29 10:43:56 +0800

    增加播放回调
    1. 初始化增加extension选项值为扩展名，增加reverse选项，true为开启倒序
    2. play和seekTo方法增加回调方法
    如frameAnimate.paly(function(){console.log('end')})
    3. 添加AMD和CMD支持，普通引用添加全局变量FrameAnimate。

--- frameAnimate.js
+++ frameAnimate.js
@@ -1,170 +1,194 @@
-'use strict';
+(function() {
+	'use strict';
 
-function frameAnimate(canvas, opts, callback) {
+	function FrameAnimate(canvas, opts, callback) {
 
-	this.canvas = canvas;
+		this.canvas = canvas;
 
-	this.width = opts.width;
-	this.height = opts.height;
-	this.frame = opts.frame;
-	this.fps = opts.fps || 24;
-	this.loop = opts.loop;
-	this.loopCount = opts.loopCount;
-	this.singleMode = opts.singleMode || false;
+		this.width = opts.width;
+		this.height = opts.height;
+		this.frame = opts.frame;
+		this.fps = opts.fps || 24;
+		this.loop = opts.loop;
+		this.loopCount = opts.loopCount;
+		this.singleMode = opts.singleMode || false;
+		this.extension = opts.extension || 'png'; //文件扩展名
+		this.reverse = opts.reverse || false; //倒叙播放
 
-	this.callback = callback;
+		this.callback = callback;
 
-	this._currentFrame = 0;
-	this.source = this.canvas.getAttribute('data-source');
-	this._setInterval;
-	this._onloadLength = 0;
+		this._currentFrame = 0;
+		this.source = this.canvas.getAttribute('data-source');
+		this._setInterval;
+		this._onloadLength = 0;
 
-	this._frames = [];
+		this._frames = [];
 
-	this.init();
+		this.init();
 
-}
-
-frameAnimate.prototype.init = function() {
-	var self = this;
-	console.log(self)
-	this.canvas.width = this.width;
-	this.canvas.height = this.height;
-
-	this._ctx = this.canvas.getContext("2d");
-
-	if (this.singleMode) {
-		singleMode();
-	} else {
-		picsMode();
 	}
-	// 未完成
-	function singleMode() {
-		self._img = new Image();
-		self._img.src = self.source;
-		self._img.onload = function() {
-			var img_WIDTH = self._img.width / self.width;
-			var img_HEIGHT = self._img.height / self.height;
-			for (var i = 0; i < img_HEIGHT; i++) {
-				for (var j = 0; j < img_WIDTH; j++) {
-					var frame = {
-						x: (j % img_WIDTH) * self.width,
-						y: i * self.height
-					};
-					self.frames.push(frame);
+
+	FrameAnimate.prototype.init = function() {
+		var self = this;
+		console.log(self)
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+
+		this._ctx = this.canvas.getContext("2d");
+
+		if (this.singleMode) {
+			singleMode();
+		} else {
+			picsMode();
+		}
+		// 未完成
+		function singleMode() {
+			self._img = new Image();
+			self._img.src = self.source;
+			self._img.onload = function() {
+				var img_WIDTH = self._img.width / self.width;
+				var img_HEIGHT = self._img.height / self.height;
+				for (var i = 0; i < img_HEIGHT; i++) {
+					for (var j = 0; j < img_WIDTH; j++) {
+						var frame = {
+							x: (j % img_WIDTH) * self.width,
+							y: i * self.height
+						};
+						self.frames.push(frame);
+					}
+				}
+			};
+		}
+
+		function picsMode() {
+			function source(i) {
+				var _img = new Image();
+				_img.src = self.source + i + '.' + self.extension;
+				self._frames.push(_img);
+
+				_img.onload = function() {
+					self._onloadLength++;
 				}
 			}
-		};
-	}
-
-	function picsMode() {
-		for (var i = 0; i < self.frame; i++) {
-			var _img = new Image();
-			_img.src = self.source + i + '.png';
-			self._frames.push(_img);
-
-			_img.onload = function() {
-				self._onloadLength++;
-			}
-		}
-	}
-};
-
-frameAnimate.prototype.draw = function() {
-	var self = this;
-	self._ctx.clearRect(0, 0, self.width, self.height);
-	var f = self._frames[self._currentFrame];
-	if (self.singleMode) {
-		self._ctx.drawImage(self._img, f.x, f.y, self.width, self.height);
-	} else {
-		self._ctx.drawImage(f, 0, 0, self.width, self.height);
-	}
-
-	if (self._currentFrame === self.frame - 1) {
-
-		self._currentFrame = 0;
-		if (!self.loop) {
-			// clearInterval(self._setInterval);
-			self.pause();
-		}
-		if (self.loopCount) {
-			if (self.loopCount > 1) {
-				self.loopCount--;
+			if (self.reverse) {
+				for (var i = self.frame; i > 0; i--) {
+					source(i - 1);
+				}
 			} else {
+				for (var i = 0; i < self.frame; i++) {
+					source(i);
+				}
+			}
+
+		}
+	};
+
+	FrameAnimate.prototype.draw = function(fn) {
+		var self = this;
+		self._ctx.clearRect(0, 0, self.width, self.height);
+		var f = self._frames[self._currentFrame];
+		if (self.singleMode) {
+			self._ctx.drawImage(self._img, f.x, f.y, self.width, self.height);
+		} else {
+			self._ctx.drawImage(f, 0, 0, self.width, self.height);
+		}
+
+		if (self._currentFrame === self.frame - 1) {
+
+			self._currentFrame = 0;
+			if (!self.loop) {
 				// clearInterval(self._setInterval);
 				self.pause();
+				if (typeof fn === 'function') {
+					fn();
+				}
 			}
-		}
-	} else {
-		self._currentFrame++;
-	}
-};
-
-frameAnimate.prototype.checkLoad = function(next, n) {
-	var self = this;
-	self._setInterval = setInterval(function() {
-		if (self._onloadLength === self.frame) {
-			// console.log("==pics load complete==");
-			clearInterval(self._setInterval);
-
-			switch(next) {
-				case 'draw':
-					self.draw();
-					break;
-				case 'seekTo':
-					self.seekTo(n);
-					break;
-				default:
-					self.play();
+			if (self.loopCount) {
+				if (self.loopCount > 1) {
+					self.loopCount--;
+				} else {
+					// clearInterval(self._setInterval);
+					self.pause();
+				}
 			}
+		} else {
+			self._currentFrame++;
 		}
-	}, 1000/this.fps);
-};
+	};
 
-frameAnimate.prototype.play = function() {
-	var self = this;
-	self.pause();
-	if (self._onloadLength < self.frame) {
-		self.checkLoad('play');
-	} else {
+	FrameAnimate.prototype.checkLoad = function(next, n, fn) {
+		var self = this;
 		self._setInterval = setInterval(function() {
 			if (self._onloadLength === self.frame) {
-				self.draw();
+				// console.log("==pics load complete==");
+				clearInterval(self._setInterval);
+
+				switch (next) {
+					case 'draw':
+						self.draw();
+						break;
+					case 'seekTo':
+						self.seekTo(n, fn);
+						break;
+					default:
+						self.play(typeof n === 'function' ? n : null);
+				}
 			}
-		}, 1000/this.fps);
-	}
-};
+		}, 1000 / this.fps);
+	};
 
-frameAnimate.prototype.pause = function() {
-	var self = this;
-	if (self._setInterval) {
-		clearInterval(self._setInterval);
-	}
-	if (self.callback) {
-		self.callback();
-	}
-};
+	FrameAnimate.prototype.play = function(fn) {
+		var self = this;
+		self.pause();
+		if (self._onloadLength < self.frame) {
+			self.checkLoad('play', fn);
+		} else {
+			self._setInterval = setInterval(function() {
+				if (self._onloadLength === self.frame) {
+					self.draw(fn);
+				}
+			}, 1000 / this.fps);
+		}
+	};
 
-frameAnimate.prototype.stop = function() {
-	var self = this;
-	self.pause();
-	self.seekTo(self.frame - 1);
-};
+	FrameAnimate.prototype.pause = function() {
+		var self = this;
+		if (self._setInterval) {
+			clearInterval(self._setInterval);
+		}
+		if (self.callback) {
+			self.callback();
+		}
+	};
 
-frameAnimate.prototype.replay = function() {
-	this.pause();
-	this.seekTo(0);
-	this.play();
-};
+	FrameAnimate.prototype.stop = function() {
+		var self = this;
+		self.pause();
+		self.seekTo(self.frame - 1);
+	};
 
-frameAnimate.prototype.seekTo = function(n) {
-	var self = this;
+	FrameAnimate.prototype.replay = function() {
+		this.pause();
+		this.seekTo(0);
+		this.play();
+	};
 
-	self.pause();
-	self._currentFrame = n;
-	if (self._onloadLength < self.frame) {
-		self.checkLoad('seekTo', n);
+	FrameAnimate.prototype.seekTo = function(n, fn) {
+		var self = this;
+
+		self.pause();
+		self._currentFrame = n;
+		if (self._onloadLength < self.frame) {
+			self.checkLoad('seekTo', n, fn);
+		} else {
+			self.draw(fn);
+		}
+	};
+	if (typeof define === 'function' && (define.amd || define.cmd)) {
+		define(function() {
+			return FrameAnimate;
+		});
 	} else {
-		self.draw();
+		window.FrameAnimate = FrameAnimate;
 	}
-};
+}());
